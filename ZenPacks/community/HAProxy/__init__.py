d8a51959f369b18a0b9dc7cb2b7abb484e3bc118
Jason Harvey <alienth@reddit.com>
2012-04-18 00:55:02 -0700

    add license header

--- ZenPacks/community/HAProxy/__init__.py
+++ ZenPacks/community/HAProxy/__init__.py
@@ -1,3 +1,14 @@
+###########################################################################
+#
+# Copyright (C) 2012, Jason Harvey
+# Based on code by Zenoss, Inc. -- Copyright (C) 2011, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
+#
+###########################################################################
+
 
 import Globals
 import os.path

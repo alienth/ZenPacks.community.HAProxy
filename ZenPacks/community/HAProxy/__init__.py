60c93293d49a416061a6b7d1b9d224e62caf96aa
Jason Harvey <alienth@reddit.com>
2012-04-18 00:57:19 -0700

    init code

--- ZenPacks/community/HAProxy/__init__.py
+++ ZenPacks/community/HAProxy/__init__.py
@@ -9,5 +9,39 @@
 #
 ###########################################################################
 
+"""
+Custom ZenPack initialization code. All code defined in this module will be
+executed at startup time in all Zope clients.
+"""
+
+import logging
+log = logging.getLogger('zen.HAProxy')
 
 import Globals
+
+from Products.ZenEvents.EventManagerBase import EventManagerBase
+from Products.ZenModel.Device import Device
+from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
+from Products.ZenRelations.RelSchema import ToManyCont, ToOne
+from Products.ZenUtils.Utils import unused
+
+unused(Globals)
+
+
+ZENPACK_NAME = 'ZenPacks.community.HAProxy'
+
+# Define new device relations.
+NEW_DEVICE_RELATIONS = (
+    ('haproxy_instances', 'HAProxyInstance'),
+    )
+
+# Add new relationships to Device if they don't already exist.
+for relname, modname in NEW_DEVICE_RELATIONS:
+    if relname not in (x[0] for x in Device._relations):
+        Device._relations += (
+            (relname, ToManyCont(ToOne,
+                '.'.join((ZENPACK_NAME, modname)), 'haproxy_host')),
+            )
+
+
+
